dc37b25e9b63391d6733a5fece2d124ca973494b
Caetano <caetano.bona@gmail.com>
2025-06-17 19:03:23 -0300

    Added dropdown menu to card

--- src/frontend/src/features/recipes/components/RecipeCard.vue
+++ src/frontend/src/features/recipes/components/RecipeCard.vue
@@ -1,19 +1,44 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 import Card from '@/components/ui/card/Card.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 const props = defineProps(['title', 'description', 'author', 'tag', 'likes'])
-
 </script>
 
 <template>
   <Card class="shadow-none border-none py-0">
     <div class="relative mb-6">
       <div class="aspect-[4/3] relative overflow-hidden bg-gray-100">
-        <img 
+        <img
           src="../../../assets/placeholder.svg"
           fill
           class="object-cover transition-minimal group-hover:scale-105"
         />
+      </div>
+      <div class="absolute top-4 right-4 flex gap-2 transition-minimal">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="h-8 w-8 bg-white/90 hover:bg-white border-0 shadow-sm cursor-pointer items-center justify-center flex"
+            >
+              <img src="../../../assets/burger-menu.svg" alt="Icon" class="w-4 h-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent class="rounded-none">
+            <DropdownMenuItem class="rounded-none">Edit</DropdownMenuItem>
+            <DropdownMenuItem class="rounded-none">Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
 
@@ -26,10 +51,8 @@ const props = defineProps(['title', 'description', 'author', 'tag', 'likes'])
           <p>by {{ author }}</p>
         </div>
         <div class="flex">
-          
           <p>{{ likes }}</p>
         </div>
-        
       </div>
     </div>
   </Card>
