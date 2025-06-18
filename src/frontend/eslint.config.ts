f922500818a9185f7e2f434f0438d4e47abd3b18
Caetano <caetano.bona@gmail.com>
2025-06-17 21:28:09 -0300

    Updated typescript-eslint to latest to fix error in eslint.config.ts

--- src/frontend/eslint.config.ts
+++ src/frontend/eslint.config.ts
@@ -12,6 +12,9 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
